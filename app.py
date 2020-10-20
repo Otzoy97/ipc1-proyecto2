87f2c5c87163cbdcc66c089b827f8c03c45a1a5c
S.Otzoy <otzoy97@hotmail.com>
2020-10-20 03:37:05 -0600

    añadido cors y wsgi

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask
+from flask_cors import CORS
 from database.db import initilize_db
 from routes.index import init_routes
 from config.config import MONGO_URI
 
 app = Flask(__name__)
+CORS(app)
 
 app.config['MONGODB_SETTINGS'] = {
     'host': MONGO_URI
@@ -12,4 +14,6 @@ app.config['MONGODB_SETTINGS'] = {
 initilize_db(app)
 init_routes(app)
 
-app.run()
+if __name__ == '__main__':
+    app.run()
+    # app.run(host='0.0.0.0', port=5000, debug=True)
