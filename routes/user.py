77b8f4042b4d0d26c533b7532e503a20e7693d41
S.Otzoy <otzoy97@hotmail.com>
2020-10-20 01:30:58 -0600

    manejo de usuario duplicado

--- routes/user.py
+++ routes/user.py
@@ -1,17 +1,30 @@
-from flask import Blueprint, Response, request
+from flask import Blueprint, Response, request, jsonify
 from mongoengine.errors import NotUniqueError
+from errors.errors import DuplicatedUser
 from models.user import User
 
 users = Blueprint('users', __name__)
 
-## Crear usuario 
-@users.route('/users', methods=['POST'])
-def add_user():
+## Signup
+@users.route('/users/signup', methods=['POST'])
+def sign_up():
     try:
         body = request.get_json()
         user = User(**body).save()
         id = user.id
         return {'id': str(id)}, 200
     except NotUniqueError:
-        raise NotUniqueError('El usuario ya existe', status_code=400)
+        raise DuplicatedUser('El usuario ya existe', status_code=400)
+
+## Login
+@users.route('/users/login', methods=['POST'])
+def log_in():
+    pass
+
+@users.errorhandler(DuplicatedUser)
+def handle_not_unique_error(error):
+    response = jsonify(error.to_dict())
+    response.status_code = error.status_code
+    return response
+
 
