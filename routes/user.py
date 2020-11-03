6d653d38684e2a41b65dc1344629b94a7cf26977
S.Otzoy <otzoy97@hotmail.com>
2020-11-03 01:01:37 -0600

    agregado crear usuario admin actualizado crear usuario cliente agregado listar usuarios

--- routes/user.py
+++ routes/user.py
@@ -8,16 +8,35 @@ from flask_jwt_extended import jwt_required, get_jwt_identity
 
 users = Blueprint('users', __name__)
 
-## Signup
-@users.route('/users/signup', methods=['POST'])
-def sign_up():
+## Signup - cliente
+@users.route('/users/client/signup', methods=['POST'])
+def normal_sign_up():
     try:
         body = request.get_json()
-        user = User(**body).save()
-        id = user.id
+        User(**body).save()
         return {'message': 'Usuario creado'}, 200
     except NotUniqueError:
         raise DuplicatedUser('El usuario ya existe', status_code=400)
+
+@users.route('/users/admin/signup', methods=['POST'])
+@jwt_required
+def admin_sign_up():
+    """Crea un nuevo usuario administrador. Solo un usuario admin
+    puede crear otro usuario admin"""
+    admin_id = get_jwt_identity()
+    user = User.objects.get(id=admin_id)
+    if user.type_ == 1:
+        try:
+            body = request.get_json()
+            user = User(**body)
+            user.type_ = 1
+            user.save()
+            return {'message': 'Usuario admin creado'}, 200
+        except NotUniqueError:
+            raise DuplicatedUser('El usuario ya existe', status_code=400)
+    else:
+        return {'message': 'El usuario no es admin'}, 401
+
 
 ## Login
 @users.route('/users/login', methods=['POST'])
@@ -25,7 +44,7 @@ def log_in():
     try:
         body = request.get_json()
         user = User.objects.get(usr=body.get('usr'), pwd=body.get('pwd'))
-        expires = datetime.timedelta(days=7)
+        expires = datetime.timedelta(days=1)
         access_token = create_access_token(identity=str(user.id), expires_delta=expires)
         return {'token': access_token}, 200
     except DoesNotExist:
@@ -52,6 +71,24 @@ def update():
         return {'message': 'Usuario actualizado'}, 200
     except NotUniqueError:
         raise DuplicatedUser('El usuario ya existe', status_code=400)
+
+@users.route('/users/admin/list', methods=['GET'])
+@jwt_required
+def list():
+    """Recupera la información de todos los usuarios"""
+    admin_id = get_jwt_identity()
+    user = User.objects.get(id=admin_id)
+    if user.type_ == 1:
+        try:
+            #users = User.objects.all()
+            users = []
+            for u in User.objects.all():
+                users.append({"nombre": u.name, "apellido": u.surname, "usuario": u.usr, "rol": "Cliente" if u.type_ == 0 else "Admin"})
+            return {"payload": users}, 200
+        except NotUniqueError:
+            raise DuplicatedUser('El usuario ya existe', status_code=400)
+    else:
+        return {'message': 'El usuario no es admin'}, 401
 
 @users.errorhandler(DuplicatedUser)
 def handle_not_unique_error(error):
