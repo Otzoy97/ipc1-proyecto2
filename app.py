da26b7e14dc32d1ea1b825dcec53f58af3c0e504
S.Otzoy <otzoy97@hotmail.com>
2020-11-02 22:54:54 -0600

    añdido soporte a jwt

--- app.py
+++ app.py
@@ -2,7 +2,8 @@ from flask import Flask
 from flask_cors import CORS
 from database.db import initilize_db
 from routes.index import init_routes
-from config.config import MONGO_URI
+from config.config import MONGO_URI, JWT_SEED
+from flask_jwt_extended import JWTManager
 
 app = Flask(__name__)
 CORS(app)
@@ -10,6 +11,8 @@ CORS(app)
 app.config['MONGODB_SETTINGS'] = {
     'host': MONGO_URI
 }
+app.config['JWT_SECRET_KEY'] = JWT_SEED
+jwt = JWTManager(app)
 
 initilize_db(app)
 init_routes(app)
