cb855dbd4356fa8915ce68fd743a4196961d1da5
S.Otzoy <otzoy97@hotmail.com>
2020-11-03 21:16:10 -0600

    modelos se fueron a un solo archivo

--- models/models.py
+++ models/models.py
@@ -1,5 +1,10 @@
 from database.db import db
 
+class Review(db.Document):
+    user = db.ReferenceField('User')
+    app = db.ReferenceField('Application')
+    txt = db.StringField()
+
 class User(db.Document):
     name = db.StringField(required=True, max_length=50)
     surname = db.StringField(required=True, max_length=50)
@@ -12,6 +17,7 @@ class Application(db.Document):
     title = db.StringField()
     url = db.URLField()
     cat = db.ReferenceField('Category')
+    reviews = db.ListField(db.ReferenceField('Review', reverse_delete_rule=db.PULL))
     downloads = db.IntField(default = 0)
     des = db.StringField()
     price = db.DecimalField(default = 0.00)
@@ -21,9 +27,7 @@ class Category(db.Document):
     name = db.StringField(unique = True)
     apps = db.ListField(db.ReferenceField('Application', reverse_delete_rule=db.PULL))
 
-class Review(db.Document):
-    user = db.ReferenceField('User')
-    txt = db.StringField()
 
 Category.register_delete_rule(Application, 'cat', db.CASCADE)
 User.register_delete_rule(Review, 'user', db.CASCADE)
+Application.register_delete_rule(Review, 'app', db.CASCADE)
